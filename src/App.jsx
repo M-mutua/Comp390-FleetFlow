dd8b7041e1b7f0da85378e88e7044a56aca9370e
Stephen Kamau <kamaustephen004@gmail.com>
2026-03-12 08:49:52 +0300

    i am recommiting my work after there was a corruption to the database due to network failure

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-teal-600">COMP 390: Group Project - FLEETFLOW</h1>
+    </div>
+  )
+}
